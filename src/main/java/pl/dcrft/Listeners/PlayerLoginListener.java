e5eada4f98f157cfbcec4b6088a25b9a9ba71270
Nick Nicker <nick4nicker@gmail.com>
2021-07-04 20:36:32 +0200

    Fix the whitelist issue properly

--- src/main/java/pl/dcrft/Listeners/PlayerLoginListener.java
+++ src/main/java/pl/dcrft/Listeners/PlayerLoginListener.java
@@ -1,12 +1,19 @@
 package pl.dcrft.Listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import pl.dcrft.DragonCraftCore;
+
+import java.awt.*;
+import java.io.File;
 
 import static pl.dcrft.Managers.LanguageManager.getMessage;
 
@@ -16,23 +23,11 @@ public class PlayerLoginListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onLogin(PlayerLoginEvent event) {
-        if(event.getAddress().toString().equals("/" + plugin.getConfig().getString("bungee.ip"))){
-            if(plugin.getServer().hasWhitelist()){
-                if(event.getPlayer().isWhitelisted()){
-                    event.allow();
-                } else {
-                    event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, (ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("bungee.kick_message"))));
-                    return;
-                }
-            } else {
-                event.allow();
-                return;
-            }
-        } else {
-            event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, (ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("bungee.kick_message"))));
+        if(!event.getAddress().toString().equals("/" + plugin.getConfig().getString("bungee.ip"))){
+            event.setResult(PlayerLoginEvent.Result.KICK_WHITELIST);
+            event.setKickMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("bungee.kick_message")));
             return;
         }
-
 
         if (event.getResult() == PlayerLoginEvent.Result.KICK_FULL) {
             Player player = event.getPlayer();
